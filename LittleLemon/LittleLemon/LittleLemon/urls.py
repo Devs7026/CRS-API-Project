8f0c3233f6adc89b841f1304c82b3adcceebcd22
Devs7026 <d2twins2607@gmail.com>
2025-01-24 14:34:36 +0530

    second commit

--- LittleLemon/LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/LittleLemon/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
-      path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/',include('LittleLemonApi.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
-  
 
